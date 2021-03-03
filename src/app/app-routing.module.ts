f6c094b5a3d464a7334ddf85cce96b933c0d5fac
farhane.hafsa <farhane.hafsa@gmail.com>
2021-03-03 15:40:31 +0100

    Les modules du front-end

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/back-office/dashboard', pathMatch: 'full'},
+  {
+    path: 'back-office',
+    loadChildren: () => import('./back-office/back-office.module').then(m => m.BackOfficeModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  {
+    path: 'front-office',
+    loadChildren: () => import('./front-office/front-office.module').then(m => m.FrontOfficeModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
