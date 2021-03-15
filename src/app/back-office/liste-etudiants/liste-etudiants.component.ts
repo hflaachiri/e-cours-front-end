48d378e1e74f2968e77bb304476dc2e8eb71d3c0
farhane.hafsa <farhane.hafsa@gmail.com>
2021-03-15 12:26:28 +0100

    list etudiants est faite

--- src/app/back-office/liste-etudiants/liste-etudiants.component.ts
+++ src/app/back-office/liste-etudiants/liste-etudiants.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import {Etudiant} from '../../core/models/etudiant';
+
+const ELEMENT_DATA: Etudiant[] = [
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+  {firstName: 'Jhon', lastName: 'DOE', email: 'john.doe@etu.univ-amu.fr'},
+];
 
 @Component({
   selector: 'app-liste-etudiants',
@@ -8,6 +22,9 @@ import { Component, OnInit } from '@angular/core';
 export class ListeEtudiantsComponent implements OnInit {
 
   constructor() { }
+
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'icons'];
+  dataSource = ELEMENT_DATA;
 
   ngOnInit(): void {
   }
